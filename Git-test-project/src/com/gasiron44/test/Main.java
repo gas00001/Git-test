a8e440353c40c080c49a93dbcd44430d4c1582b7
gas00001 <gas00001@yandex.ru>
2016-12-24 11:50:58 +0300

    I have just created a new java project

---
+++ Git-test-project/src/com/gasiron44/test/Main.java
@@ -0,0 +1,18 @@
+package com.gasiron44.test;
+/**
+ * 
+ */
+
+/**
+ * @author GAS
+ *
+ */
+public class Main {
+	
+	public static void main(String[] args) {
+		
+		System.out.println("Hello from Git Hub!!!");
+		
+	}
+
+}
